55dea7a00885c8c78a2faef63fec0c47a14a8768
Sean Davis <aaaathatisfouras@gmail.com>
2021-02-01 21:20:41 -0500

    Initial Revision

---
+++ PermutationPalindrome/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PermutationPalindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
